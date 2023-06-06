0fb61dab9cc0938568b60751ff67f688d2144c44
RadiazOm <jeffrey.otterloo@gmail.com>
2023-06-06 15:48:30 +0200

    debug mode off

--- src/js/scenes.js
+++ src/js/scenes.js
@@ -18,7 +18,7 @@ export class Scenes extends Engine {
             displayMode: DisplayMode.FitScreen
         });
         this.setAntialiasing(false)
-        this.showDebug(true)
+        this.showDebug(false)
         this.start(ResourceLoader).then(() => this.startGame());
     }
 
